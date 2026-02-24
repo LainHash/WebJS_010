7476483a39ce7ecbd1ab4d2b121506cf365125f8
Lain <ctythanhhien1974@gmail.com>
2026-02-24 09:21:44 +0700

    web 03

--- web/src/pages/product/Products.tsx
+++ web/src/pages/product/Products.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
-import Navbar from "../components/Navbar";
-import Footer from "../components/Footer";
+import Navbar from "../../components/Navbar";
+import Footer from "../../components/Footer";
 import ProductGrid from "~/components/ProductGrid";
 
 function ProductsPage() {
--- web/src/routes.ts
+++ web/src/routes.ts
@@ -2,9 +2,9 @@ import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
 export default [
   index("pages/Home.tsx"),
-  route("products", "pages/Products.tsx"),
-  route("products/:id", "pages/ProductDetail.tsx"),
-  route("products/laptop/:id", "pages/LaptopDetail.tsx"),
-  route("products/cpu/:id", "pages/CpuDetail.tsx"),
-  route("products/gpu/:id", "pages/GpuDetail.tsx"),
+  route("contact", "pages/Contact.tsx"),
+  route("products", "pages/product/Products.tsx"),
+  route("products/laptop/:id", "pages/product/LaptopDetail.tsx"),
+  route("products/cpu/:id", "pages/product/CpuDetail.tsx"),
+  route("products/gpu/:id", "pages/product/GpuDetail.tsx"),
 ] satisfies RouteConfig;
