a8ae0eb3b708978304dccb21c2960514bf91db95
Lain <ctythanhhien1974@gmail.com>
2026-02-24 21:37:13 +0700

    test

--- server/src/app.js
+++ server/src/app.js
@@ -8,8 +8,8 @@ app.use(cors());
 app.use(express.json());
 app.use("/api", router);
 
-// app.get("/", (req, res) => {
-//   res.send("Hello, World");
-// });
+app.get("/", (req, res) => {
+  res.send("Hello, World");
+});
 
 export default app;
